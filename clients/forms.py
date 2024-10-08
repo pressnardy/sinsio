8ba4bb45cc5c20e956d2fdbf675e6b02935f7660
7thscihub <7thscihub@gmail.com>
2024-10-08 10:15:54 +0300

    mobile home page

--- clients/forms.py
+++ clients/forms.py
@@ -2,18 +2,20 @@ from django import forms
 from clients.models import Client
 from django.contrib.auth.models import User
 
+# instantiated form for editing the client model and the required fields
 class ClientForm(forms.ModelForm):
 
 	class Meta:
 		model = Client
 
 		fields = [
-			"id", "name", "policy_number", "registration_number",
+			"id", "name", "policy_number", "registration_number", "phone_number",
 			"start_date", "end_date", "renewal_date", "amount", 'created_by',
 	  	]
 
+		#adding CSS classes and IDs for forms fields for use in front end
 		widgets = {
-			# "id": forms.HiddenInput(attrs={'style': 'display: none;'}),
+			
 			"name": forms.TextInput(attrs={
 				'class': 'input-text',
 				'id': 'client-name',
@@ -26,6 +28,7 @@ class ClientForm(forms.ModelForm):
 				'class': 'input-text',
 				'id': 'registration-number',
 			}),
+			
 			"start_date": forms.TextInput(attrs={
 				'type': 'date', 'class': 'input-date',
 				'id': 'start-date',
@@ -41,6 +44,10 @@ class ClientForm(forms.ModelForm):
 			}),
 			"created_by": forms.TextInput(attrs={
 				'class': 'input-text', 'placeholder': 'Amount', 'id': 'amount',
+			}),
+			"phone_number": forms.TextInput(attrs={
+				'class': 'input-text',
+				'id': 'phone-number',
 			}),
 		}
 
--- clients/models.py
+++ clients/models.py
@@ -12,10 +12,11 @@ class Client(models.Model):
 	end_date = models.DateField(null=True)
 	renewal_date = models.DateField(null=True)
 	amount = models.DecimalField(max_digits=10, decimal_places=2)
+	phone_number = models.CharField(max_length=20, null=True, blank=True)
 	created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None, blank=True)
 
 
 	def __str__(self):
 		return f"{self.name}, {self.policy_number}, {self.registration_number}, " \
-			   f"{self.start_date}, {self.end_date}, {self.amount}"
+			   f"{self.start_date}, {self.end_date}, {self.amount}, {self.phone_number}"
 
